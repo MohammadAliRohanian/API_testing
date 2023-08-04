6fcafba3b6d0d52f911d0dfb7dbb18ab0a59340e
unknown <rohanian0803@gmail.com>
2023-08-05 01:06:00 +0330

    feat(route)!: create route file

--- scenarios/sample.py
+++ scenarios/sample.py
@@ -1,17 +1,19 @@
-import os
+# import os
 
-from os import environ
+# from os import environ
 
 import sys   
 sys.path.append('B:/Code/api_testing_v1') 
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from api_methods.get import get_req
 from api_methods.post import post_req
 from api_methods.put import put_req
 from api_methods.delete import delete_req
+
+import route
 
 # end_point = "https://64abbfd29edb4181202e72ce.mockapi.io/users"
 # post_data = {"name" : "MohammadAli", "family" : "Rohanian", "email" : "rohanian0803@gmail.com"}
@@ -19,9 +21,9 @@ from api_methods.delete import delete_req
 # put_data = {"name" : "MohammadAli"}
 # delete_end_point = "https://64abbfd29edb4181202e72ce.mockapi.io/users/2"
 
-# post_req(end_point, post_data)
-# put_req(put_end_point, put_data)
-# delete_req(delete_end_point)
-# get_req(end_point)
+post_req(route.END_POINT, route.POST_DATA)
+put_req(route.PUT_END_POINT, route.PUT_DATA)
+delete_req(route.DELETE_END_POINT)
+get_req(route.END_POINT)
 
-print(os.environ['END_POINT'])
+# print(os.environ['END_POINT'])
